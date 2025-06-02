acff38d1b4fa26d5232567c2e231f4368a275305
Sumit Nirmal <sumitramprakashnirmal@gmail.com>
2025-06-02 11:31:24 +0530

    waitlist from update

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
 // env.js
 window.env = {
-  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw5GtQmBbUYwyTGpcYVyQlgS8fkbrq-RdknYO1U5P5rabvjhV2-ChT39_eTWQPG4ywO/exec",
+  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzHHdBYRE0q0R_6-qczKuJD-eSbQ2cPPNQofsDIbxNfXfcqUSy_1pFIvHxKjJWwVhvg/exec",
 };
